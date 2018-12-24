734d2a59987ab9bb0531bde133ba468049078da9
Boris Guzner <github@regmagik.com>
2018-12-24 14:59:58 -0500

    Update index.php

--- key/index.php
+++ key/index.php
@@ -1,14 +1,17 @@
 <?php
 
-echo ("Thank you for your interest in RegmagiK."); 
+echo ("Thank you for visiting RegmagiK."); 
 
+echo ("<br>Before Name2SN..."); 
 function Name2SN($name)
 {
+echo ("Inside Name2SN()..."); 
 	$sn = getenv('KEYSECRET');
 	$len = mb_strlen($name);
 	for ($i = 0; $i < $len; $i++)
 	{
 	  $letter = mb_substr($name, $i, 1);
+	  echo($i . ":" . $letter);
 	  if($letter == " ") continue;
 	  $value = mb_convert_encoding($letter, 'UTF-16');
 	  $b = ord($value[1])&0xff;
@@ -17,6 +20,7 @@ function Name2SN($name)
 	}
 	return $sn;
 }
+echo ("mb_internal_encoding. UTF-8..."); 
 mb_internal_encoding("UTF-8");
 $name = $_GET["initals"] ." ". $_GET["name"] ." ". $_GET["email"];
 //$name = "Cédric GEORGEOT cgeorgeot@e-novatic.fr";
